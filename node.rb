ba287c5d9ba7167282607a123af666bf2d2c22b9
Lucy Fox <lucyreneefox@gmail.com>
2016-03-30 23:17:10 -0600

    Deleted some excess comments

--- node.rb
+++ node.rb
@@ -7,20 +7,6 @@ attr_accessor :rating, :title, :left, :right
     @left = left_child #these are less than
     @right = right_child #these are greater than
   end
-
-#array1.is_equal(array2)
-  def is_equal?(rating)
-    @rating == rating
-  end
-
-  def is_greater?(rating)
-    @rating > rating
-  end
-
-  def is_less_than?(rating)
-    @rating < rating
-  end
-
 #   def rating
 #     @rating
 #   end
@@ -37,36 +23,10 @@ attr_accessor :rating, :title, :left, :right
 #     @right_child
 #   end
 #
-#
-#
 #   #^these are the things a node knows
 # #what are the questions I can ask this node?
 # #node, what is your rating?
 # #node, what is your title?
 # #node, do you have a left_child?
 # #node, do you have a right_child?
-#
-#
-
-
-  #this will return until our current_node.next_node == nil
-  #so we return this to our methods and give them a node to work with
-  #methods decide what to do with that node:
-  #aka insert, depth_of, max, min, etc.....
-
-
 end
-
-
-
-# store in new node and push to another method to place
-#
-# new_node <= root
-# #go to the left child
-# elsif new_node >= root
-# #go to the right child
-# end
-
-#pull out boolean logic into small methods
-# def count
-#   return 1 if no head node
